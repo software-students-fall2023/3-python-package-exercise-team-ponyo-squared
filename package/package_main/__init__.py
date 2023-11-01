df1fd4ed5e8553cf0c76f52c92f79291a6a1e738
Jhon Kim <yk2606@nyu.edu>
2023-11-01 13:53:58 -0400

    functions

---
+++ package/package_main/__init__.py
@@ -0,0 +1,47 @@
+import time # Import the time module for timing functions
+import functools # Import the functools module for working with decorators
+
+
+# Dictionary to store function runtimes
+function_runtimes = {}
+
+
+def tracker(func):
+@functools.wraps(func) # Use functools.wraps to preserve the original function's metadata
+def wrapper(*args, **kwargs):
+start_time = time.time() # Record the current time before executing the decorated function
+result = func(*args, **kwargs) # Execute the decorated function
+end_time = time.time() # Record the current time after executing the decorated function
+elapsed_time = end_time - start_time # Calculate the elapsed time
+function_name = func.__name__ # Get the name of the decorated function
+function_runtimes[function_name] = elapsed_time # Store the runtime in the dictionary
+return result # Return the result of the decorated function
+return wrapper # Return the wrapper function (the actual decorator)
+
+
+def printAll():
+for function_name, runtime in function_runtimes.items():
+print(f"{function_name}: {runtime:.2f} seconds") # Print the name and runtime of each function
+
+
+def saveToFile(file_name):
+with open(file_name, 'w') as file:
+for function_name, runtime in function_runtimes.items():
+file.write(f"{function_name}: {runtime:.2f} seconds\n") # Write function names and runtimes to a file
+
+
+# Decorator to print the runtime of all functions
+def AllRuntime(func):
+@functools.wraps(func) # Use functools.wraps to preserve the original function's metadata
+def wrapper(*args, **kwargs):
+start_time = time.time() # Record the current time before executing the decorated function
+result = func(*args, **kwargs) # Execute the decorated function
+end_time = time.time() # Record the current time after executing the decorated function
+elapsed_time = end_time - start_time # Calculate the elapsed time
+
+
+print("Function Runtimes:") # Print a header for the function runtimes
+printAll() # Use the printAll() function to print the runtimes of all functions
+print(f"Total Program Runtime: {elapsed_time:.2f} seconds") # Print the total program runtime
+return result # Return the result of the decorated function
+return wrapper # Return the wrapper function (the actual decorator)
--- package/package_main/main.py
+++ package/package_main/main.py
@@ -1,3 +1,17 @@
-def say_hello(name):
-    """Greet someone by name."""
-    return f"Hello, {name}!"
+from package_main import tracker, printAll, saveToFile, AllRuntime
+
+
+@tracker
+def example_function():
+# TBW
+
+
+@AllRuntime
+def main():
+example_function()
+# can also put the functions to be tested here
+
+
+if __name__ == "__main__":
+main()
+
