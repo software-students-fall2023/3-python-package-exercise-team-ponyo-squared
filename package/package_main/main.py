b6e332e25cbb8e62e2238fdedc35337bf909155f
Jhon Kim <yk2606@nyu.edu>
2023-11-01 17:25:57 -0400

    everything is functional

--- package/package_main/main.py
+++ package/package_main/main.py
@@ -17,17 +17,30 @@ def tracker(func):
         return result # returning the result of the decorated function
     return wrapper # returning the wrapper function (the actual decorator)
 
+def tracker_print(func):
+    @functools.wraps(func) # using functools.wraps to preserve the original function's metadata
+    def wrapper(*args, **kwargs):
+        start_time = time.time() # recording the current time before executing the decorated function
+        result = func(*args, **kwargs) # executing the decorated function
+        end_time = time.time() # recording the current time after executing the decorated function
+        elapsed_time = end_time - start_time # calculating the elapsed time
+        function_name = func.__name__ # getting the name of the decorated function
+        function_runtimes[function_name] = elapsed_time # storing the runtime in the dictionary
+        print(f"Runtime of {function_name}: {elapsed_time * 1000:.4f} milliseconds")
+        return result # returning the result of the decorated function
+    return wrapper # returning the wrapper function (the actual decorator)
+
 
 def printAll():
     for function_name, runtime in function_runtimes.items():
-        print(f"-> {function_name}: {runtime * 1000:.2f} milliseconds")
+        print(f"{function_name}: {runtime * 1000:.4f} milliseconds")
         #print(f"{function_name}: {runtime:.2f} seconds") # Print the name and runtime of each function
 
 
 def saveToFile(file_name):
     with open(file_name, 'w') as file:
         for function_name, runtime in function_runtimes.items():
-            file.write(f"{function_name}: {runtime * 1000:.2f} milliseconds\n")
+            file.write(f"{function_name}: {runtime * 1000:.4f} milliseconds\n")
             #file.write(f"{function_name}: {runtime:.2f} seconds\n") # Write function names and runtimes to a file
 
 # # decorator to print the runtime of all functions
@@ -46,7 +59,7 @@ def saveToFile(file_name):
 #         return result # Return the result of the decorated function
 #     return wrapper # Return the wrapper function (the actual decorator)
 
-def AllRuntime(func):
+def entire_Runtime(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
--- package/tests/_test.py
+++ package/tests/_test.py
@@ -1,5 +1,24 @@
-from package_main.main import say_hello
+from package_main.main import tracker, tracker_print, printAll, entire_Runtime
 
-def test_say_hello():
-    assert say_hello("Alex") == "Hello, Alex!"
-    assert say_hello("") == "Hello, !"
+@tracker
+def example_function():
+    for i in range(1000):
+        i = i * 2
+    return("Test function executed")
+
+@tracker_print
+def example_function2():
+    for i in range(10000):
+        i = i * 2
+    return("Test function executed")
+
+@entire_Runtime
+def test_example_function():
+    result = example_function()
+    result2 = example_function2()
+    assert result == "Test function executed"
+    assert result2 == "Test function executed"
+    print("example_function executed successfully")
+
+# def test_printAll():
+#     printAll()
