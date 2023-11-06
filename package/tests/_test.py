0e75ee381862b51b64c28a68dc1f31b6c0ffeb29
as13909 <as13909@nyu.edu>
2023-11-06 15:02:37 -0500

    more test cases

--- package/tests/_test.py
+++ package/tests/_test.py
@@ -17,76 +17,88 @@ def example_function3():
         i -=1
 
 
-
-
 @tracker
 def test_example_function():
     result = example_function()
     result2 = example_function2()
     result3 = example_function3()
 
+@tracker
 def test_printAll():
     print_all_runtimes()
     print("\n-------------------------------------------------------------------------------------------------\n")
 
+@tracker
 def test_printAll2(): 
     save_to_file("test_printAll.txt")
     print_all_runtimes() 
     print("\n-------------------------------------------------------------------------------------------------\n")
-    
+
+@tracker
 def test_printall3():
     tracker_print(example_function2())
     print_all_runtimes()
     print("\n-------------------------------------------------------------------------------------------------\n")
-
+@tracker
 def test_printall4():
     print_all_runtimes()
     entire_runtime()
     print("\n-------------------------------------------------------------------------------------------------\n")
-    
+
+@tracker
 def test_saveTo1():
     #testing saving to a file that doesn't exist
     save_to_file("test_saveTo.txt")
     #giving the file some example info to put in
     entire_runtime()
     print("\n-------------------------------------------------------------------------------------------------\n")
+@tracker
 def test_saveTo2():
     #testing saving to a file that already exists
     save_to_file("test_saveTo.txt")
     #saving nothing
     print("\n-------------------------------------------------------------------------------------------------\n")
+@tracker
 def test_saveTo3():
     #testing saving to a file that already exists
     save_to_file("test_saveTo.txt")
     #saving some info
     entire_runtime()
     print("\n-------------------------------------------------------------------------------------------------\n")
+@tracker
 def test_saveTo4():
     print_fancy_runtime(5)#random number
     save_to_file("test_saveTo.txt")
     print_fancy_runtime(2)#random number
     #I.E. save_to_file is unaffected by other prints before or after its run
     print("\n-------------------------------------------------------------------------------------------------\n")
-
+@tracker
 def test_trackerprint1():
     #testing it on just one function
     tracker_print(example_function())
     print("\n-------------------------------------------------------------------------------------------------\n")
-
+@tracker
 def test_trackerprint2():
     #testing it on multiple functions
     tracker_print(example_function())
     tracker_print(example_function2())
     print("\n-------------------------------------------------------------------------------------------------\n")
-
+@tracker
 def test_trackerprint3():
     #testing it with non related functions
     tracker_print(example_function())
     print_all_runtimes()
     print("\n-------------------------------------------------------------------------------------------------\n")
-
+@tracker
 def test_trackerprint2():
     #testing it with file saving functions
     save_to_file("test_trackerprint.txt")
     tracker_print(example_function())
+    print("\n-------------------------------------------------------------------------------------------------\n")
+
+@tracker
+def test_trackerprint2():
+    #testing it with file saving functions
+    print("HERE")
+    print_fancy_runtime(500)
     print("\n-------------------------------------------------------------------------------------------------\n")
