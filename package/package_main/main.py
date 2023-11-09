93c0b3ac3091917a1760537d5876f145ce486a35
Jhon Kim <yk2606@nyu.edu>
2023-11-08 19:31:09 -0500

    updated pypi username on github & checking

--- package/package_main/main.py
+++ package/package_main/main.py
@@ -59,22 +59,6 @@ def save_to_file(file_name):
             file.write(f"{function_name}: {runtime * 1000:.4f} milliseconds\n")
             #file.write(f"{function_name}: {runtime:.2f} seconds\n") # Write function names and runtimes to a file
 
-# # decorator to print the runtime of all functions
-# def AllRuntime(func):
-#     @functools.wraps(func) # Use functools.wraps to preserve the original function's metadata
-#     def wrapper(*args, **kwargs):
-#         start_time = time.time() # Record the current time before executing the decorated function
-#         result = func(*args, **kwargs) # Execute the decorated function
-#         end_time = time.time() # Record the current time after executing the decorated function
-#         elapsed_time = end_time - start_time # Calculate the elapsed time   
-
-#         print("Function Runtimes:") # Print a header for the function runtimes
-#         printAll() # Use the printAll() function to print the runtimes of all functions
-#         print(f"Total Program Runtime: {elapsed_time * 1000:.2f} milliseconds") # Print the total program runtime
-#         #print(f"Total Program Runtime: {elapsed_time:.2f} seconds") # Print the total program runtime
-#         return result # Return the result of the decorated function
-#     return wrapper # Return the wrapper function (the actual decorator)
-
 def entire_runtime():
         end_time = time.time()
         elapsed_time = end_time - start_time
